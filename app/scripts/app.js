b97f5ae3aa436910f1fecc4c04fda7c3d027b5e7
hiroqn <hiroqn1008@gmail.com>
2015-05-02 23:55:22 +0900

    feat(js): add api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,12 @@
 angular.module('kamikikaiApp', ['duScroll'])
-  .controller('MainCtrl', function ($scope, $timeout, $element) {
+  .controller('MainCtrl', function ($scope, $timeout, $http) {
     var diff = Date.now() - ps;
     $timeout(function () {
       $scope.loaded = true;
     }, 500 - diff > 0 ? 500 - diff : 0);
+
+    $scope.members = [];
+    $http.get('/api/members').success(function (members) {
+      $scope.members = members;
+    });
   });
