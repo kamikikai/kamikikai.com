feb9702a45ca6efecc150d59ebc22e99b20a0e01
hiroqn <hiroqn1008@gmail.com>
2015-06-15 04:53:00 +0900

    chore(app): finish

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,7 @@ angular.module('kamikikaiApp', ['duScroll'])
     var World = Matter.World;
     var Bodies = Matter.Bodies;
     var Events = Matter.Events;
+
     $scope.members = [];
     $http.get('/api/members').success(function (members) {
       var engine = Engine.create($element[0].querySelector('.members'), {
@@ -22,30 +23,38 @@ angular.module('kamikikaiApp', ['duScroll'])
       });
       $scope.members = members;
       var bodies = members.map(function (member, i) {
-        var body = Bodies.circle(270 + (Math.random() - .5) * 24, i * -50 , 48, {
+        var body = Bodies.circle(270 + (Math.random() - .5) * 24, i * -50, 48 * 1.2, {
           angle: i,
           render: {
-            strokeStyle: '#d04030',
             sprite: {
               texture: member.icon,
               xScale: 0.5,
               yScale: 0.5
             }
-          }});
+          }
+        });
+        body.member = member;
         return body;
       });
       World.add(engine.world, bodies);
       World.add(engine.world, [
-        Bodies.rectangle(270, 540, 540, 1, {isStatic: true}),
-        Bodies.rectangle(540, 270, 1, 540, {isStatic: true}),
-        Bodies.rectangle(0, 270, 1, 540, {isStatic: true})
+        Bodies.rectangle(270, 542, 540, 2, {isStatic: true}),
+        Bodies.rectangle(542, 270, 2, 540, {isStatic: true}),
+        Bodies.rectangle(-2, 270, 2, 540, {isStatic: true})
       ]);
-      var mouseConstraint = Matter.MouseConstraint.create(engine, {
+      Events.on(engine, 'mousedown', function (event) {
+        var body = bodies.filter(function (body) {
+          return Matter.Vertices.contains(body.vertices, event.mouse.position);
+        })[0];
+        if (body) {
+          body.torque +=10;
+          body.force.y -=.5;
 
-      });
-      Events.on(engine, 'tick', function(event) {
-        //if(mouseConstraint.mouse.button == 0){
-        //}
+          $scope.$apply(function () {
+            $scope.member = body.member;
+
+          });
+        }
       });
       Engine.run(engine);
     });
